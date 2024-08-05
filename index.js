90b3224bac9f59b0d80ffca09a164c2f76ab0669
Sahareior Sijan <sahareiors@gmail.com>
2024-08-06 01:05:41 +0600

    minor fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: 'https://neonnet.netlify.app/',
+  origin: 'https://neonnet.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allow credentials
